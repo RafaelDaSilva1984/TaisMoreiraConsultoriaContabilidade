826fad11dc1aa44e6a9e8e511f4b7081a5d44949
Rafael Da Silva <rfsrafaelsilva@gmail.com>
2025-09-09 17:42:12 -0300

    add novo, backgrounf e introducao

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const cabecalho = document.querySelector(".header-bg");
+const logo_marca = document.querySelector(".header-img");
+
+function mudarCorBordaLogo(event) {
+  logo_marca.style.border = "solid 2px  rgba(168, 98, 70, 0.8)";
+  logo_marca.style.borderRadius = "5rem";
+}
+function retornaCorBordaLogo(event) {
+  logo_marca.style.border = "solid 2px #fff";
+}
+logo_marca.addEventListener("mouseover", mudarCorBordaLogo);
+logo_marca.addEventListener("mouseout", retornaCorBordaLogo);
