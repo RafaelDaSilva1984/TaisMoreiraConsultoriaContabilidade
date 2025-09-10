900502db39ef039cbd4f1e152d14f89e6722a2ef
Rafael Da Silva <rfsrafaelsilva@gmail.com>
2025-09-10 17:54:28 -0300

    update cores detalhes e botões

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const links = document.querySelectorAll(".header-menu a");
 const logo_marca = document.querySelector(".header-img");
 
 function mudarCorBordaLogo(event) {
-  logo_marca.style.border = "solid 2px  rgba(168, 98, 70, 0.8)";
+  logo_marca.style.border = "solid 1px  rgba(0, 119, 181,0.2)";
   logo_marca.style.borderRadius = "5rem";
 }
 function retornaCorBordaLogo(event) {
